3e401f91ad82ccab0b5bb869b051802de2dff567
leeseonseonje <leeseonje9323@gmail.com>
2022-03-12 22:37:09 +0900

    Initial commit

---
+++ src/baekjoon/NO1026.java
@@ -0,0 +1,4 @@
+package baekjoon;
+
+public class NO1026 {
+}
---
+++ src/baekjoon/NO9095.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class NO9095 {
+    static int[] result = new int[1000];
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            logic((scanner.nextInt()));
+        }
+
+
+    }
+
+    static void logic(int n) {
+        result[1] = 1;
+        result[2] = 2;
+        result[3] = 4;
+
+        for (int i = 4; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+        }
+        System.out.println(result[n]);
+    }
+}
+
