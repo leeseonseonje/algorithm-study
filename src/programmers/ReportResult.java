e0b7531c56563ba046dfb35b5439a4f3e7eae6bc
leeseonseonje <leeseonje9323@gmail.com>
2022-03-29 00:01:50 +0900

    report

--- src/programmers/ReportResult.java
+++ src/programmers/ReportResult.java
@@ -1,8 +1,6 @@
 package programmers;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
@@ -14,19 +12,88 @@ public class ReportResult {
 
     public static void main(String[] args) {
 
-        Map<String, Integer> map = new HashMap<>();
+        Set<Report> reports = Arrays.stream(report)
+                .map(r -> new Report(reportSplit(r, 0), reportSplit(r, 1)))
+                .collect(toSet());
 
-        for (int i = 0, n = report.length; i < n; i++) {
-            Integer count = map.getOrDefault(reportSplit(report[i],1), 0);
-            map.put(reportSplit(report[i], 1), ++count);
+        System.out.println(reports);
+
+        Map<String, Integer> map = new HashMap<>();
+        for (Report r : reports) {
+            Integer count = map.getOrDefault(r.getTroll(), 0);
+            map.put(r.getTroll(), ++count);
         }
 
         map.keySet().forEach(e -> System.out.println(e + " : " + map.get(e)));
 
+        Map<String, Integer> m = new HashMap<>();
+
+        for (String s : map.keySet()) {
+            if (map.get(s) >= k) {
+                for (Report r : reports) {
+                    if (s.equals(r.getTroll())) {
+                        Integer count = m.getOrDefault(r.getCleanUser(), 0);
+                        m.put(r.getCleanUser(), ++count);
+                    }
+                }
+            }
+        }
+
+        m.keySet().forEach(e -> System.out.println(e + " : " + m.get(e)));
+        int[] result = new int[id_list.length];
+
+        for (int i = 0; i < result.length; i++) {
+            if (m.get(id_list[i]) == null) {
+                result[i] = 0;
+            } else {
+                result[i] = m.get(id_list[i]);
+            }
+            System.out.println(result[i]);
+        }
 
     }
 
     public static String reportSplit(String report, int index) {
         return report.split(" ")[index];
+    }
+}
+
+class Report {
+
+    private String cleanUser;
+    private String troll;
+
+    public Report(String cleanUser, String troll) {
+        this.cleanUser = cleanUser;
+        this.troll = troll;
+    }
+
+    public String getCleanUser() {
+        return cleanUser;
+    }
+
+    public String getTroll() {
+        return troll;
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "cleanUser='" + cleanUser + '\'' +
+                ", troll='" + troll + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return Objects.equals(cleanUser, report.cleanUser) && Objects.equals(troll, report.troll);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cleanUser, troll);
     }
 }
