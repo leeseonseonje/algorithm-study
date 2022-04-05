79c03c20371797c229f79a8038fae4c03fc9240d
leeseonseonje <leeseonje9323@gmail.com>
2022-04-06 00:22:55 +0900

    keypad

--- src/programmers/level1/keypad/Keypad.java
+++ src/programmers/level1/keypad/Keypad.java
@@ -1,13 +1,13 @@
 package programmers.level1.keypad;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Keypad {
 
-    public static int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-    public static String hand = "right";
+    public static int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+    public static String hand = "left";
 
     public static void main(String[] args) {
 
@@ -15,9 +15,12 @@ public class Keypad {
         List<String> list = new ArrayList<>();
 
         int leftIndex = 10;
-        int rightIndex = 100;
+        int rightIndex = 12;
 
         for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 0) {
+                numbers[i] = 11;
+            }
             if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
                 list.add("L");
                 leftIndex = numbers[i];
@@ -25,22 +28,12 @@ public class Keypad {
                 list.add("R");
                 rightIndex = numbers[i];
             } else {
-                System.out.println("LEFT " + leftIndex);
-                System.out.println("RIGHT " + rightIndex);
-                System.out.println("INDEX " + numbers[i]);
-                int left = Math.abs(leftIndex - numbers[i]);
-                int right = Math.abs(rightIndex - numbers[i]);
-                if (left == 1 || left == 3) {
-                    if (hand.equals("left")) {
-                        list.add("L");
-                        leftIndex = numbers[i];
-                    }
-                } else if (right == 1 || right == 3) {
-                    if (hand.equals("right")) {
-                        list.add("R");
-                        rightIndex = numbers[i];
-                    }
-                } else if (left == right) {
+                int left = (Math.abs((leftIndex - numbers[i])) / 3) + (Math.abs((leftIndex - numbers[i])) % 3);
+                int right = (Math.abs((rightIndex - numbers[i])) / 3) + (Math.abs((rightIndex - numbers[i])) % 3);
+                System.out.println("left = " + left);
+                System.out.println("right = " + right);
+                System.out.println("numbers = " + numbers[i]);
+                if (left == right) {
                     if (hand.equals("left")) {
                         list.add("L");
                         leftIndex = numbers[i];
@@ -48,44 +41,18 @@ public class Keypad {
                         list.add("R");
                         rightIndex = numbers[i];
                     }
-                } else if (left == 2 && right != 2) {
-                    list.add("R");
-                    rightIndex = numbers[i];
-                } else {
+                } else if (left == 1) {
+                    System.out.println("numbers22 = " + numbers[i]);
                     list.add("L");
                     leftIndex = numbers[i];
+                } else {
+                    list.add("R");
+                    rightIndex = numbers[i];
                 }
             }
         }
-        list.forEach(System.out::print);
-        // LRLLLRLLRRL 답
-        // LRLLLRLLLR 내
+        String collect = list.stream().collect(Collectors.joining());
+        System.out.println(collect);
     }
 }
 
-enum Hand {
-    L (1, 4, 7),
-    R (3, 6, 9);
-
-    private int a;
-    private int b;
-    private int c;
-
-    Hand(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public int getA() {
-        return a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public int getC() {
-        return c;
-    }
-}
