4f6eee9787740eec4edad09860abb0a401ec67d4
seonje <leeseonje9323@gmail.com>
2022-08-24 00:10:03 +0900

    구현문제 상하좌우

---
+++ src/thisiscodingtest/implement/UpDownLeftRight.java
@@ -0,0 +1,46 @@
+package thisiscodingtest.implement;
+
+import java.util.Scanner;
+
+public class UpDownLeftRight {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int row = 0;
+        int column = 0;
+
+        String direction = "";
+        for (int i = 0; i <= 6; i++) {
+            direction = scan.nextLine();
+            switch (direction) {
+                case "L":
+                    if (column - 1 > 0) {
+                        column--;
+                    }
+                    break;
+                case "R":
+                    if (column + 1 <= n) {
+                        column++;
+                        System.out.println(column);
+                    }
+                    break;
+                case "D":
+                    if (row + 1 <= n) {
+                        row++;
+                        System.out.println(row);
+                    }
+                    break;
+                default:
+                    if (row - 1 > 0) {
+                        row--;
+                        System.out.println(row);
+                    }
+                    break;
+            }
+        }
+        System.out.println((row+1) + "," + (column+1));
+    }
+}
